be939299b8441e01b5cd7b0584e1c8cdd28571b7
Tyler Fabian <tylerf808@gmail.com>
2023-06-07 15:56:25 -0400

    Dropped the carousel library for custom switch statement that returns proper slide

--- client/src/routes/SignUp.jsx
+++ client/src/routes/SignUp.jsx
@@ -116,153 +116,46 @@ export default function SignUp({ showAlert, loggedIn, setLoggedIn, setUser, setC
     }
   }
 
-  return (
-    <div className="pageContainer">
-      <div className="headerContainer">
-        <h1>Create an Account</h1>
-      </div>
-      {/* <CarouselProvider
-        className="carousel"
-        id='carousel'
-        naturalSlideWidth={100}
-        naturalSlideHeight={125}
-        totalSlides={4}
-        dragEnabled={false}
-        touchEnabled={false}
-      >
-        <Slider className="slider">
-          <Slide className="slide" index={0}>
-            <div className="slideItem">
-              <p className="slideLabel">Email:</p>
-              <input onChange={(e) => setEmail(e.target.value)} value={email} id="email-signup" className="textInput" type="email" />
-              <p className="slideLabel">Password:</p>
-              <input onChange={(e) => setPassword(e.target.value)} value={password} id="password-signup" className="textInput" type="password" />
-              <p className="slideLabel">Confirm Password:</p>
-              <input id="password-signup-conf" className="textInput" type="password" />
-              <div style={{ position: 'relative', left: 80 }}>
-                <p style={{ marginRight: 5, marginTop: 6, fontSize: 15 }}>Show Password</p>
-                <input style={{ position: 'relative', left: 80 }} onClick={togglePassword} type='checkbox'></input>
-              </div>
-            </div>
-          </Slide>
-          <Slide className="slide" index={1}>
-            <div className="slideItem">
-              <p style={{ margin: 8, fontSize: 20 }}>On the next several pages you'll be asked to input your costs for your operation.</p>
-              <p style={{ margin: 8, fontSize: 20 }}>Some will be percentages
-                and some will be fixed costs. These costs will be used to calculate profitability on your new jobs.</p>
-              <p style={{ margin: 8, fontSize: 20 }}> Fuel costs and
-                tolls are calculated by the system based on the route.
-              </p>
-            </div>
-          </Slide>
-          <Slide className="slide" index={2}>
-            <div className="slideItem">
-              <p className="slideLabel">How often do you pay insurance?</p>
-              <select value={insuranceFreq} onChange={(e) => setInsuranceFreq(e.target.value)}>
-                <option value='monthly'>Monthly</option>
-                <option value='bi-monthly'>Bi-Monthly</option>
-                <option value='quarterly'>Quarterly</option>
-                <option value='annually'>Annually</option>
-              </select>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">How much is you insurance payment?</p>
-              <input onChange={(e) => setInsuranceAmount(e.target.value)} value={insuranceAmount} id="insurance-amount" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter your monthly tractor lease payment.</p>
-              <input onChange={(e) => setTractorAmount(e.target.value)} value={tractorAmount} id="tractor-amount" className="newCostInput" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter your monthly trailer lease payment.</p>
-              <input onChange={(e) => setTrailerAmount(e.target.value)} value={trailerAmount} id='trailer-amount' className="newCostInput" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter dispatch fee as percentage of revenue.</p>
-              <input onChange={(e) => setDispatchAmount(e.target.value)} value={dispatchAmount} id='dispatch-amount' className="newCostInput percentageInput" type='number'></input>
-              <p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter factor fee as percentage of revenue.</p>
-              <input onChange={(e) => setFactorAmount(e.target.value)} value={factorAmount} id="factor-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter other direct costs (ODC) as a percentage to cover incidental costs.</p>
-              <input onChange={(e) => setOdcAmount(e.target.value)} value={odcAmount} id="odc-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter monthly loan payments if any.</p>
-              <input onChange={(e) => setLoanAmount(e.target.value)} value={loanAmount} id="loan-amount" className="newCostInput" type='number'></input>
-            </div>
-          </Slide>
-           <Slide className="slide" index={3}>
-            
-          </Slide>
+
+  switch(currentSlide){
+    case 0:
+      return (
+        <div>
+          <div className="headerContainer">
+            <h1>Create an Account</h1>
+          </div>
+          <p>slide 1</p>
+          <div className="btnContainer">
           
-          <Slide className="slide" index={4}>
-            <div className="slideItem">
-              <p className="slideLabel">Enter factor fee as percentage of revenue.</p>
-              <input onChange={(e) => setFactorAmount(e.target.value)} value={factorAmount} id="factor-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter other direct costs (ODC) as a percentage to cover incidental costs.</p>
-              <input onChange={(e) => setOdcAmount(e.target.value)} value={odcAmount} id="odc-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">Enter monthly loan payments if any.</p>
-              <input onChange={(e) => setLoanAmount(e.target.value)} value={loanAmount} id="loan-amount" className="newCostInput" type='number'></input>
-            </div>
-          </Slide>
-          <Slide className="slide" index={5}>
-            <div className="slideItem">
-              <p className="slideLabel">How much do you spend on average for repairs each month?</p>
-              <input onChange={(e) => setRepairsAmount(e.target.value)} value={repairsAmount} id="repairs-amount" className="newCostInput" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">How much do you put aside for depreciation each month?</p>
-              <input onChange={(e) => setDepreciationAmount(e.target.value)} value={depreciationAmount} id="depreciation-amount" className="newCostInput" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">What is your MPG?</p>
-              <input onChange={(e) => setMpgAmount(e.target.value)} value={mpgAmount} id="mpg-amount" className="newCostInput" type='number'></input>
-            </div>
-          </Slide>
-          <Slide className="slide" index={6}>
-            <div className="slideItem">
-              <p className="slideLabel">How much do you spend on average on G&A each month?</p>
-              <input onChange={(e) => setGandaAmount(e.target.value)} value={gandaAmount} id="ganda-amount" className="newCostInput" type='number'></input>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">What is your labor rate?</p>
-              <input onChange={(e) => setLaborAmount(e.target.value)} value={laborAmount} id="labor-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-            <div className="slideItem">
-              <p className="slideLabel">What percentage do you pay in payroll tax?</p>
-              <input onChange={(e) => setPayrollAmount(e.target.value)} value={payrollAmount} id="payroll-amount" className="newCostInput percentageInput" type='number'></input><p className="percentageSign">%</p>
-            </div>
-          </Slide> 
-        </Slider>
-        <div className="btnContainer">
-          <ButtonBack className="btn1" onClick={() => {
-            const newSlide = currentSlide - 1
-            setCurrentSlide(newSlide)
-            setLastSlide(false)
-          }} style={{ margin: 10, height: 30, width: 50 }}>Back</ButtonBack>
-          {lastSlide ?
-            <button onClick={createAccount} style={{ margin: 10, height: 30, width: 60, fontWeight: 'bold' }} className="btn1">Submit</button>
-            :
-            <ButtonNext className="btn1" onClick={() => {
-              const newSlide = currentSlide + 1
-              setCurrentSlide(newSlide)
-              if (newSlide === 5) {
-                setLastSlide(true)
-              }
-            }} style={{ margin: 10, height: 30, width: 50 }}>Next</ButtonNext>}
+          <button onClick={() => {
+            if(currentSlide !== 0)
+              setCurrentSlide(currentSlide -1)
+          }}>Back</button>
+          <button onClick={() => {
+            setCurrentSlide(currentSlide + 1)
+          }}>Next</button>
+          </div>
+          <div className="headerContainer">
+            <p style={{ marginTop: 50 }}>Already have an account? <Link to='/'>Log in here!</Link></p>
+          </div>
+        </div>)
+      break;
+      case 1:
+        return(
+          <div>
+          <div className="headerContainer">
+            <h1>Create an Account</h1>
+          </div>
+          <p>slide 2</p>
+
+          <div className="headerContainer">
+            <p style={{ marginTop: 50 }}>Already have an account? <Link to='/'>Log in here!</Link></p>
+          </div>
         </div>
-      </CarouselProvider> */}
-      <div>
-        <p style={{ marginTop: 50 }}>Already have an account? <Link to='/'>Log in here!</Link></p>
-      </div>
-    </div>
-  );
+        )
+      break;
+      default:
+
+  }
+
 }
